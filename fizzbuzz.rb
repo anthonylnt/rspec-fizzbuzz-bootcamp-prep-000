9175430b39267217fdd7045b67052f3f57f85c45
Anthony Elijah <lionnet.ant@gmail.com>
2018-04-22 14:11:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
-  puts "Fizz""
-else 
+    puts "Fizz"
+  elsif int % 5 == 0
+   puts "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+   puts "FizzBuzz"
+  else
+    puts "nil"
   end
-  
 end
